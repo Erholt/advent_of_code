5eaea43fe2a9691f9b25e0d7893f0b46d72a9ec4
Jacob Erholt <jacob@erholt.dk>
2026-09-04 14:50:51 +0200

    extract solution_1 into a object
    finish day 2, and work on better describing the tests

--- javascript/2015/2/script.js
+++ javascript/2015/2/script.js
@@ -3,24 +3,39 @@ const path = require("node:path");
 
 let input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").split("\n");
 
-function solution_1(input) {
-  return input.map(line => {
-    let [l, w, h]    = line.split("x").map(Number);
-    let surface_area = (2 * l * w) + (2 * w * h) + (2 * h * l);
-    let slack        = Math.min(l * w, w * h, h * l);
-  
-    return surface_area + slack;
-  }).reduce((sum, value) => sum + value, 0);
+function solution_1 (input) {
+  return input.map(line => { return gift(line).wrapping_paper })
+              .reduce((sum, value) => sum + value, 0);
 }
 
-function solution_2(input) {
-  return "Hello World!";
+function solution_2 (input) {
+  return input.map(line => { return gift(line).combined_ribbon })
+              .reduce((sum, value) => sum + value, 0);
 }
 
-console.log(`Day 2, Solution 1: ${solution_1(input)}`);
-console.log(`Day 2, Solution 2: ${solution_2(input)}`);
+function gift(dimensions) {
+  let [l, w, h]          = dimensions.split("x").map(Number);
+  let dimensions_numbers = [l, w, h];
+
+  // Wrapping paper
+  let surface_area   = (2 * l * w) + (2 * w * h) + (2 * h * l);
+  let slack          = Math.min(l * w, w * h, h * l);
+  let wrapping_paper = surface_area + slack;
+
+  // Ribbon
+  let sorted_dimensions = dimensions_numbers.slice().sort((a, b) => a - b);
+  let ribbon            = 2 * (sorted_dimensions[0] + sorted_dimensions[1]);
+  let bow_tie           = l * w * h;
+  let combined_ribbon   = ribbon + bow_tie;
+
+  return {
+    wrapping_paper,
+    combined_ribbon
+  };
+}
 
 module.exports = {
   solution_1,
-  solution_2
+  solution_2,
+  gift
 };
--- javascript/2015/2/test.js
+++ javascript/2015/2/test.js
@@ -1,20 +1,62 @@
 const test   = require("node:test");
 const assert = require("node:assert");
+const fs     = require("node:fs");
+const path   = require("node:path");
 
-const { solution_1, solution_2 } = require("./script");
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").split("\n");
+const { solution_1, solution_2, gift } = require("./script");
 
-// Solution 1
+test.describe("Day 2", () => {
+  // Solution 1
+  test.describe("Solution 1", () => {
+    test("solution_1 returns 58", () => {
+      assert.strictEqual(solution_1(["2x3x4"]), 58);
+    });
 
-test ("solution_1 returns 58'", () => {
-  assert.strictEqual(solution_1(["2x3x4"]), 58);
+    test("solution_1 returns 43", () => {
+      assert.strictEqual(solution_1(["1x1x10"]), 43);
+    });
+
+    test("solution_1 returns correct value with given input", () => {
+      assert.strictEqual(solution_1(input), 1588178);
+    });
+  });
+
+  // Solution 2
+  test.describe("Solution 2", () => {
+    test("solution_2 returns 34", () => {
+      assert.strictEqual(solution_2(["2x3x4"]), 34);
+    });
+
+    test("solution_2 returns 14", () => {
+      assert.strictEqual(solution_2(["1x1x10"]), 14);
+    });
+
+    test("solution_2 returns correct value with given input", () => {
+      assert.strictEqual(solution_2(input), 3783758);
+    });
+  });
+
+  // Gift Object
+  test.describe("Gift Object", () => {
+    test.describe("wrapping paper", () => {
+      test("returns correct amount for 2x3x4", () => {
+        assert.strictEqual(gift("2x3x4").wrapping_paper, 58);
+      });
+
+      test("returns correct amount for 1x1x10", () => {
+        assert.strictEqual(gift("1x1x10").wrapping_paper, 43);
+      });
+    });
+
+    test.describe("ribbon", () => {
+      test("returns correct amount for 2x3x4", () => {
+        assert.strictEqual(gift("2x3x4").combined_ribbon, 34);
+      });
+
+      test("returns correct amount for 1x1x10", () => {
+        assert.strictEqual(gift("1x1x10").combined_ribbon, 14);
+      });
+    });
+  });
 });
-
-test ("solution_1 returns 43'", () => {
-  assert.strictEqual(solution_1(["1x1x10"]), 43);
-});
-
-// Solution 2
-
-//test ("solution_2 returns 'Hello World!'", () => {
-//  assert.strictEqual(solution_2("test"), "Hello World!");
-//});
