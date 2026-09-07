1525ff37ad3a27011f5a868049c1dfe7794ccf19
Erholt <jacob@erholt.dk>
2026-09-07 14:07:30 +0200

    Make the layout of the day 2 test look as day 1

--- javascript/2015/2/test.js
+++ javascript/2015/2/test.js
@@ -10,22 +10,34 @@ test.describe("Day 2", () => {
   // Solution 1
   test.describe("Solution 1", () => {
     test("solution_1 returns 58", () => {
-      assert.strictEqual(solution_1(["2x3x4"]), 58);
+      assert.strictEqual(
+        solution_1(["2x3x4"]),
+        58
+      );
     });
 
     test("solution_1 returns 43", () => {
-      assert.strictEqual(solution_1(["1x1x10"]), 43);
+      assert.strictEqual(
+        solution_1(["1x1x10"]),
+        43
+      );
     });
   });
 
   // Solution 2
   test.describe("Solution 2", () => {
     test("solution_2 returns 34", () => {
-      assert.strictEqual(solution_2(["2x3x4"]), 34);
+      assert.strictEqual(
+        solution_2(["2x3x4"]),
+        34
+      );
     });
 
     test("solution_2 returns 14", () => {
-      assert.strictEqual(solution_2(["1x1x10"]), 14);
+      assert.strictEqual(
+        solution_2(["1x1x10"]),
+        14
+      );
     });
   });
 
@@ -33,21 +45,33 @@ test.describe("Day 2", () => {
   test.describe("Gift Object", () => {
     test.describe("wrapping paper", () => {
       test("returns correct amount for 2x3x4", () => {
-        assert.strictEqual(gift("2x3x4").wrapping_paper, 58);
+        assert.strictEqual(
+          gift("2x3x4").wrapping_paper,
+          58
+        );
       });
 
       test("returns correct amount for 1x1x10", () => {
-        assert.strictEqual(gift("1x1x10").wrapping_paper, 43);
+        assert.strictEqual(
+          gift("1x1x10").wrapping_paper,
+          43
+        );
       });
     });
 
     test.describe("ribbon", () => {
       test("returns correct amount for 2x3x4", () => {
-        assert.strictEqual(gift("2x3x4").combined_ribbon, 34);
+        assert.strictEqual(
+          gift("2x3x4").combined_ribbon,
+          34
+        );
       });
 
       test("returns correct amount for 1x1x10", () => {
-        assert.strictEqual(gift("1x1x10").combined_ribbon, 14);
+        assert.strictEqual(
+          gift("1x1x10").combined_ribbon,
+          14
+        );
       });
     });
   });
