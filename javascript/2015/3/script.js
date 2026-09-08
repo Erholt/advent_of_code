7044fac82c215fd1bb4f17bcc801f8fe1accb806
Jacob Erholt <jacob@erholt.dk>
2026-09-08 03:51:56 +0200

    further clean up and extract bot solutions into a seperate function which works with both

--- javascript/2015/3/script.js
+++ javascript/2015/3/script.js
@@ -3,53 +3,37 @@ const path = require("node:path");
 
 let input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").trim();
 
-function solution_1(input) {
-  let santa                     = coordinates();
-  let array_of_houses_with_present = [santa.to_string()];
+function solution_1(input) { return count_unique_houses(input, [coordinates()]); };
+function solution_2(input) { return count_unique_houses(input, [coordinates(), coordinates()]); };
+
+function count_unique_houses(input, movers) {
+  let visited = [movers[0].to_string()];
 
   for (let i = 0; i < input.length; i++) {
-    switch (input[i]) {
-      case "^": santa.go_north(); break;
-      case "v": santa.go_south(); break;
-      case ">": santa.go_east(); break;
-      case "<": santa.go_west(); break;
-    }
-    array_of_houses_with_present.push(santa.to_string());
-  }
+    let mover = movers[i % movers.length];
 
-  return [...new Set(array_of_houses_with_present)].length;
-}
-
-function solution_2(input) {
-  let santa                        = coordinates();
-  let robo_santa                   = coordinates();
-  let array_of_houses_with_present = [santa.to_string()];
-
-  for (let i = 0; i < input.length; i++) {
-    let mover = i % 2 == 0 ? santa : robo_santa;
-      
     switch (input[i]) {
       case "^": mover.go_north(); break;
       case "v": mover.go_south(); break;
       case ">": mover.go_east(); break;
       case "<": mover.go_west(); break;
     }
-    
-    array_of_houses_with_present.push(mover.to_string());
+
+    visited.push(mover.to_string());
   }
 
-  return [...new Set(array_of_houses_with_present)].length;
+  return [...new Set(visited)].length;
 }
 
 function coordinates() {
-  let current_position = { x: 0, y: 0 }
+  let current_position = { x: 0, y: 0 };
 
-  function to_string() { return `${current_position.x},${current_position.y}` }
+  function to_string() { return `${current_position.x},${current_position.y}` };
 
-  function go_north() { current_position.y += 1 }
-  function go_south() { current_position.y -= 1 }
-  function go_east()  { current_position.x += 1 }
-  function go_west()  { current_position.x -= 1 }
+  function go_north() { current_position.y += 1 };
+  function go_south() { current_position.y -= 1 };
+  function go_east()  { current_position.x += 1 };
+  function go_west()  { current_position.x -= 1 };
 
   return {
     current_position,
@@ -58,8 +42,8 @@ function coordinates() {
     go_south,
     go_east,
     go_west
-  }
-}
+  };
+};
 
 module.exports = {
   solution_1,
