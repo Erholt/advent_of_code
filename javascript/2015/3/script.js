cac01f2f057f10f0f80377e845129f4edd4dd210
Jacob Erholt <jacob@erholt.dk>
2026-09-08 03:00:16 +0200

    create coordinates object and add tests

--- javascript/2015/3/script.js
+++ javascript/2015/3/script.js
@@ -4,11 +4,52 @@ const path = require("node:path");
 let input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").trim();
 
 function solution_1(input) {
+  let array_of_houses_with_present = []
+
+  for (let i = 0; i < input.length; i++) {
+    switch (input[i]) {
+      case "^":
+        console.log("North")
+        break;
+      case "v":
+        console.log("South")
+        break;
+      case ">":
+        console.log("East")
+        break;
+      case "<":
+        console.log("West")
+        break;
+    } 
   }
+
+  return [...new Set(array_of_houses_with_present)].length
+}
+
 function solution_2(input) {
   return "World Hello!";
 }
+
+function coordinates() {
+  let current_position = { x: 0, y: 0 }
+
+  function go_north() { current_position.y += 1 }
+  function go_south() { current_position.y -= 1 }
+  function go_east()  { current_position.x += 1 }
+  function go_west()  { current_position.x -= 1 }
+
+  return {
+    current_position,
+    go_north,
+    go_south,
+    go_east,
+    go_west
+  }
+}
+
+
 module.exports = {
   solution_1,
   solution_2,
+  coordinates
 };
---
+++ javascript/2015/3/test.js
@@ -0,0 +1,132 @@
+const test   = require("node:test");
+const assert = require("node:assert");
+const fs     = require("node:fs");
+const path   = require("node:path");
+
+const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").trim();
+const { solution_1, solution_2, coordinates } = require("./script");
+
+
+test.describe("Day 3", () => {
+  // Solution 1
+  test.describe("Solution 1", () => {
+    test ("solution_1 returns the correct", () => {
+      assert.strictEqual(
+        solution_1(">"),
+        2
+      );
+
+      assert.strictEqual(
+        solution_1("^>v<"),
+        4
+      );
+
+      assert.strictEqual(
+        solution_1("^v^v^v^v^v"),
+        2
+      );
+    });
+  });
+
+  // Solution 2
+  // test.describe("Solution 2", () => {
+  //   test ("solution_2 returns 'World Hello!'", () => {
+  //     assert.strictEqual(
+  //       solution_2("test"),
+  //       "World Hello!"
+  //     );
+  //   });
+  // });
+
+ // Coordinates Object
+  test.describe("Coordinates", () => {
+    test.describe("#current_position", () => {
+      test("returns correct position", () => {
+        assert.deepStrictEqual(
+          coordinates().current_position,
+          { x: 0, y: 0 }
+        );
+      });
+
+      test("returns correct position after multiple directions", () => {
+        let position = coordinates();
+
+        position.go_north();
+        position.go_north();
+        position.go_east();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: 1, y: 2 }
+        );
+      });
+    });
+
+    test.describe("#go_north", () => {
+      test("#go_north moves the coordinates north by one", () => {
+        let position = coordinates();
+
+        position.go_north();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: 0, y: 1 }
+        );
+      });
+
+      test("multiple uses of #go_north moves the coordinates north by two", () => {
+        let position = coordinates();
+
+        position.go_north();
+        position.go_north();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: 0, y: 2 }
+        );
+      });
+    });
+
+    test.describe("#go_south", () => {
+      test("#go_south moves the coordinates south by one", () => {
+        let position = coordinates();
+
+        position.go_south();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: 0, y: -1 }
+        );
+      });
+    });
+    
+    test.describe("#go_east", () => {
+      test("#go_east moves the coordinates east by one", () => {
+        let position = coordinates();
+
+        position.go_east();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: 1, y: 0 }
+        );
+      });
+    });
+
+    test.describe("#go_west", () => {
+      test("#go_west moves the coordinates west by one", () => {
+        let position = coordinates();
+
+        position.go_west();
+
+        assert.deepStrictEqual(
+          position.current_position,
+          { x: -1, y: 0 }
+        );
+      });
+    });
+  });  
+});
+
+console.log(`Day 3, Solution 1: ${solution_1(input)}`);
+console.log(`Day 3, Solution 2: ${solution_2(input)}`);
