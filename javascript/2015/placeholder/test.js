6ba256edb409c526b198ff71ecfabfbed85770e2
Jacob Erholt <jacob@erholt.dk>
2026-09-08 03:59:25 +0200

    Make the placeholder have the same layout as the other days

--- javascript/2015/placeholder/test.js
+++ javascript/2015/placeholder/test.js
@@ -11,14 +11,20 @@ test.describe("Placeholder", () => {
   // Solution 1
   test.describe("Solution 1", () => {
     test ("solution_1 returns 'Hello World!'", () => {
-      assert.strictEqual(solution_1("test"), "Hello World!");
+      assert.strictEqual(
+        solution_1("test"),
+        "Hello World!"
+      );
     });
   }); 
 
   // Solution 2
   test.describe("Solution 2", () => {
     test ("solution_2 returns 'World Hello!'", () => {
-      assert.strictEqual(solution_2("test"), "World Hello!");
+      assert.strictEqual(
+        solution_2("test"),
+        "World Hello!"
+      );
     });
   });
 });
