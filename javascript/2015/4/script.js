c24aeaef61859db5d20e020ddc3ca4d4db488118
Jacob Erholt <jacob@erholt.dk>
2026-09-08 04:38:35 +0200

    JS 2015-4: add basic script

---
+++ javascript/2015/4/script.js
@@ -0,0 +1,17 @@
+const fs   = require("node:fs");
+const path = require("node:path");
+
+let input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").split("\n").map(line => line.trim());
+
+function solution_1(input) {
+  return "Hello World!";
+};
+
+function solution_2(input) {
+  return "World Hello!";
+};
+
+module.exports = {
+  solution_1,
+  solution_2
+};
