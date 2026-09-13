6329d5f75351b920db36e7f208d0f1a52d77ec6c
Erholt <jacob@erholt.dk>
2026-09-13 13:39:07 +0200

    JS 2015-5: add tests for #apply_rules

--- javascript/2015/5/test.js
+++ javascript/2015/5/test.js
@@ -4,7 +4,7 @@ const fs     = require("node:fs");
 const path   = require("node:path");
 
 const input = fs.readFileSync(path.join(__dirname, "input.txt"), "utf8").trim();
-const { solution_1, solution_2 } = require("./script");
+const { solution_1, solution_2, apply_rules } = require("./script");
 
 
 test.describe("Day 5", () => {
@@ -62,4 +62,47 @@ test.describe("Day 5", () => {
   //     );
   //   });
   // });
+
+  // Apply Rules
+  test.describe("Apply Rules", () => {
+    test ("apply_rules returns true for valid string", () => {
+      assert.strictEqual(
+        apply_rules("aaa"),
+        true
+      );
+    });
+
+    test ("apply_rules returns false for invalid string (String contains bad combinations)", () => {
+      assert.strictEqual(
+        apply_rules("aaab"),
+        false
+      );
+      assert.strictEqual(
+        apply_rules("aaacd"),
+        false
+      );
+      assert.strictEqual(
+        apply_rules("aaapq"),
+        false
+      );
+      assert.strictEqual(
+        apply_rules("aaaxy"),
+        false
+      );
+    });
+
+    test ("apply_rules returns false for invalid string (No double letter)", () => {
+      assert.strictEqual(
+        apply_rules("jchzalrnumimnmhp"),
+        false
+      );
+    });
+
+    test ("apply_rules returns false for invalid string (Contains only 1 vowel)", () => {
+      assert.strictEqual(
+        apply_rules("dvszwmarrgswjxmb"),
+        false
+      );
+    });
+  });
 });
