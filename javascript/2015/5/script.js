7b8ba9218bb8a27d69c99f6119afc28ae2a13a9b
Erholt <jacob@erholt.dk>
2026-09-13 13:47:41 +0200

    JS 2015-5: extract rule matching into lets
    extract the rules matching into the creation of the lets

--- javascript/2015/5/script.js
+++ javascript/2015/5/script.js
@@ -12,13 +12,9 @@ function solution_2(input) {
 };
 
 function apply_rules(string) {
-  let twice_in_a_row        = false;
-  let at_least_three_vowels = false;
-  let no_bad_combinations   = false;
-
-  if (string.match(/([a-z])\1/))                 { twice_in_a_row        = true; }
-  if (string.match(/[aeiou].*[aeiou].*[aeiou]/)) { at_least_three_vowels = true; }
-  if (string.match(/ab|cd|pq|xy/))               { no_bad_combinations   = false; } else { no_bad_combinations = true; }
+  let twice_in_a_row        = string.match(/([a-z])\1/);
+  let at_least_three_vowels = string.match(/[aeiou].*[aeiou].*[aeiou]/);
+  let no_bad_combinations   = !string.match(/ab|cd|pq|xy/);
 
   if (twice_in_a_row && at_least_three_vowels && no_bad_combinations) {
     return true;
